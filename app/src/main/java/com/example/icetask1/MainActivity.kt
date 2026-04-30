185029abdfea5078498645a226c249498eea61cb
katleho <khutsoanek20@gmail.com>
2026-04-30 11:41:05 +0200

    Completed ICE Task 2: Set content view to MainAppScreen

--- app/src/main/java/com/example/icetask1/MainActivity.kt
+++ app/src/main/java/com/example/icetask1/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.icetask1.ui.theme.ICETASK1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +14,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ICETASK1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Task 2: Correctly setting the content view of the application
+                MainAppScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ICETASK1Theme {
-        Greeting("Android")
-    }
+fun MainAppScreen() {
+    // Entry point for the application functionality
+    Text(text = "ICE Task 2: Content View Set Successfully")
 }
